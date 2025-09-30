d6bbdef679e706e93f0bb4a25e2d878402476eed
Basar Ökke <baok@weller.lan>
2025-09-30 16:35:10 +0200

    AuthLayout Loading Spinner added

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,11 +1,24 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-page-container>
+    <q-page-container class="bg-auth">
       <router-view />
     </q-page-container>
+    <q-inner-loading
+      :showing="authStore.isAuthProceeding.loading"
+      :label="authStore.isAuthProceeding.label"
+      label-style="font-size: 1.1em"
+    />
   </q-layout>
 </template>
 
 <script setup>
-// kein Drawer, kein Echo, kein Header etc.
+import { useAuthStore } from 'src/stores/auth-store';
+const authStore = useAuthStore()
+
 </script>
+
+<style scoped>
+.bg-auth{
+  background: linear-gradient(183deg, #f9ce70, #ff840057);
+}
+</style>
